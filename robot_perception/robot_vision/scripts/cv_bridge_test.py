a945afe484ec1ec6c3b7098267525578f76b7939
yinlei <tongku2008@126.com>
2020-06-24 11:33:10 +0800

    不使用类，使用函数来回调

--- robot_perception/robot_vision/scripts/cv_bridge_test.py
+++ robot_perception/robot_vision/scripts/cv_bridge_test.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import rospy
 import cv2
+
+import rospy
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+
 
 class image_converter:
     def __init__(self):    
@@ -35,12 +37,39 @@ class image_converter:
         except CvBridgeError as e:
             print e
 
+def image_convert_callback(data):
+    # 使用cv_bridge将ROS的图像数据转换成OpenCV的图像格式
+    try:
+        cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+        print e
+
+    # 在opencv的显示窗口中绘制一个圆，作为标记
+    (rows,cols,channels) = cv_image.shape
+    if cols > 60 and rows > 60 :
+        cv2.circle(cv_image, (60, 60), 30, (0,0,255), -1)
+
+    # 显示Opencv格式的图像
+    cv2.imshow("Image window", cv_image)
+    cv2.waitKey(3)
+
+    # 再将opencv格式额数据转换成ros image格式的数据发布
+    try:
+        image_pub.publish(bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+        print e
+
 if __name__ == '__main__':
     try:
         # 初始化ros节点
         rospy.init_node("cv_bridge_test")
         rospy.loginfo("Starting cv_bridge_test node")
-        image_converter()
+
+        bridge = CvBridge()
+        image_pub = rospy.Publisher("cv_bridge_image", Image, queue_size=1)
+        image_sub = rospy.Subscriber("/usb_cam/image_raw", Image, image_convert_callback)
+
+        # image_converter()
         rospy.spin()
     except KeyboardInterrupt:
         print "Shutting down cv_bridge_test node."
